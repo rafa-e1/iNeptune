aee84120a9ae5cc45f66d9da036929879386283a
MinwooJe <gpatration77@gmail.com>
2023-09-20 14:30:52 +0900

    Initial Commit

---
+++ WeeklyMission1st/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeeklyMission1st
+//
+//  Created by 제민우 on 2023/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
