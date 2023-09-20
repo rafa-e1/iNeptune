52dda482230f349344392b407adeda9e8aaa55f4
MinwooJe <gpatration77@gmail.com>
2023-09-20 22:12:29 +0900

    feat: baeminOne Heading 구현

--- WeeklyMission1st/Views/DeliverMainView.swift
+++ WeeklyMission1st/Views/DeliverMainView.swift
@@ -56,12 +56,60 @@ final class DeliverMainView: UIScrollView {
         return stackView
     }()
     
+    private let baeminOneContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private let baeminOneMainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "알뜰-한집배달"
+        label.font = UIFont(name: "BMHANNAProOTF", size: 20)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    
+    private let baeminOneSubLabel: UILabel = {
+        let label = UILabel()
+        label.text = "배민1"
+        label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    
+    private let baeminOneDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "원하는 대로 골라 주문!"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    
+    private let baeminOneLabelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.alignment = .fill
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+    
+    private let baeminOneButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
     
     private let containerStackView: UIStackView = {
        let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 10
-        stackView.alignment = .fill
+        stackView.alignment = .center
         stackView.distribution = .fillProportionally
         return stackView
     }()
@@ -82,15 +130,19 @@ final class DeliverMainView: UIScrollView {
         
         let viewWidth = Int(self.frame.width)
         
-        // 30: stackViewLeading/trailingAnchorConstant ,20: stackViewSpacing
+        // 30: stackViewLeading/trailingAnchorConstant, 20: stackViewSpacing
         let categoryButtonHeight = CGFloat((viewWidth - 30 - 20) / 3)
         
         couponAdImageView.clipsToBounds = true
         couponAdImageView.layer.cornerRadius = couponAdImageView.frame.height / 8
 
         categoryButtonStackView.heightAnchor.constraint(equalToConstant: categoryButtonHeight).isActive = true
+        
+        baeminOneContainerView.clipsToBounds = true
+        baeminOneContainerView.layer.cornerRadius = baeminOneContainerView.frame.height / 15
     }
 }
+
 // MARK: Configure Initial Setting
 extension DeliverMainView {
     private func configureInitialSetting() {
@@ -103,8 +155,9 @@ extension DeliverMainView {
     private func configureSubviews() {
         
         [couponAdImageView,
-         categoryButtonStackView
-        
+         categoryButtonStackView,
+         baeminOneContainerView,
+         
         ].forEach {
             containerStackView.addArrangedSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
@@ -112,6 +165,16 @@ extension DeliverMainView {
         
         [categoryDeliverButton, categoryBMartButton, categoryStoreButton].forEach {
             categoryButtonStackView.addArrangedSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        [baeminOneMainLabel, baeminOneSubLabel, baeminOneDescriptionLabel].forEach {
+            baeminOneLabelStackView.addArrangedSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        [baeminOneLabelStackView, baeminOneButton].forEach {
+            baeminOneContainerView.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
         
@@ -126,6 +189,7 @@ extension DeliverMainView {
     private func configureLayout() {
         let safeArea = safeAreaLayoutGuide
         
+        baeminOneLabelStackView.setCustomSpacing(5, after: baeminOneSubLabel)
         NSLayoutConstraint.activate([
             // MARK: mainSearchBar Constraints
             mainSearchBar.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.06),
@@ -147,6 +211,33 @@ extension DeliverMainView {
             // MARK: categoryButtonStackView Constraints
             categoryButtonStackView.centerXAnchor.constraint(equalTo: centerXAnchor),
             categoryButtonStackView.leadingAnchor.constraint(equalTo: containerStackView.leadingAnchor),
+            
+            // MARK: savingOrOnlyDeliveryContainerView Constraints
+            baeminOneContainerView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            baeminOneContainerView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.25),
+            baeminOneContainerView.leadingAnchor.constraint(equalTo: containerStackView.leadingAnchor),
+            
+            // MARK: savingOrOnlyDeliveryLabel Constraints
+            baeminOneLabelStackView
+                .topAnchor.constraint(
+                equalTo: baeminOneContainerView.topAnchor,
+                constant: 15
+            ),
+            baeminOneLabelStackView.leadingAnchor.constraint(
+                equalTo: baeminOneContainerView.leadingAnchor,
+                constant: 15
+            ),
+            baeminOneLabelStackView.trailingAnchor.constraint(
+                equalTo: baeminOneContainerView.trailingAnchor,
+                constant: -80
+            ),
+            
+            // MARK: baeminOneButton Constraints
+            baeminOneButton.centerYAnchor.constraint(equalTo: baeminOneLabelStackView.centerYAnchor),
+            baeminOneButton.trailingAnchor.constraint(
+                equalTo: baeminOneContainerView.trailingAnchor,
+                constant: -20
+            ),
         ])
     }
 }
