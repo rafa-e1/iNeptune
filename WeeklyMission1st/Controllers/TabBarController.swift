1f8fc0b756928fc43485ad0fd20a2d14089f1bf8
MinwooJe <gpatration77@gmail.com>
2023-09-26 05:22:41 +0900

    feat: 탭바 구현

---
+++ WeeklyMission1st/Controllers/TabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarController.swift
+//  WeeklyMission1st
+//
+//  Created by 제민우 on 2023/09/24.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
