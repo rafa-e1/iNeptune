3b31664c0263f813ceee2bc1a870a44a6937e243
MinwooJe <gpatration77@gmail.com>
2023-10-02 20:45:46 +0900

    Initial Commit

---
+++ 레오:제민우/2주차/2주차 위클리미션/2주차 위클리미션/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2주차 위클리미션
+//
+//  Created by 제민우 on 2023/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
