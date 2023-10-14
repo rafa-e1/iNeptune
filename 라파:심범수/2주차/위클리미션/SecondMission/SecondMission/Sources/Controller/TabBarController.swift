746a3cb76c9a89a2014b286ca2b10348fcef2a41
Rafael <rafa71802937@gmail.com>
2023-10-14 18:03:21 +0900

    2주차 위클리 미션

---
+++ 라파:심범수/2주차/위클리미션/SecondMission/SecondMission/Sources/Controller/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  SecondMission
+//
+//  Created by Rafael on 10/14/23.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+    
+}
