85459908fa1eb1a8d783136864aed04a0b663a3a
Rafael <rafa@Rafaelui-MacBookPro.local>
2023-10-22 21:38:44 +0900

    4주차 위클리 미션

---
+++ 라파:심범수/4주차/위클리미션/FourthMission/Sources/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  FourthMission
+//
+//  Created by Rafael on 10/20/23.
+//
+
+import SwiftUI
+
+struct HomeViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeViewController()
+}
