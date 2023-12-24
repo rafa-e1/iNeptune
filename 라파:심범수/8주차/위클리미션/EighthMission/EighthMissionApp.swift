3ffcec73627d068c9a76ba9a4697e80d68e9a4d0
Rafa <rafa71802937@gmail.com>
2023-12-24 09:37:08 +0900

    8주차 위클리 미션

---
+++ 라파:심범수/8주차/위클리미션/EighthMission/EighthMissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EighthMissionApp.swift
+//  EighthMission
+//
+//  Created by Rafael on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct EighthMissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
