b369d43de0909094ec22b599bc3b8d4bf1057cb8
Rafa <rafa71802937@gmail.com>
2023-12-26 17:05:06 +0900

    9주차 위클리 미션

---
+++ 라파:심범수/9주차/위클리미션/NinthMission/NinthMissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NinthMissionApp.swift
+//  NinthMission
+//
+//  Created by Rafael on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct NinthMissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ 라파:심범수/9주차/위클리미션/NinthMission/UserInfo.swift
@@ -0,0 +1,13 @@
+//
+//  UserInfo.swift
+//  NinthMission
+//
+//  Created by Rafael on 12/24/23.
+//
+
+import Foundation
+
+struct UserInfo {
+    var id: String
+    var pw: String
+}
